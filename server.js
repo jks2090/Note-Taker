b9cb650b91d6a7048315c1cd5b9c3331f84a6275
Joseph Strickland <jks2090@outlook.com>
2020-11-18 10:48:39 -0500

    updated code

---
+++ server.js
@@ -0,0 +1,109 @@
+
+var express = require("express");
+const fs = require("fs");
+var path = require("path");
+var newArray = [];
+var app = express();
+
+
+var PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+app.listen(PORT, function () {
+    console.log(`App is listening on http://localhost:${PORT}`);
+});
+
+
+
+
+app.get("/", function (req, res) {
+
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+
+
+
+app.get("/notes", function (req, res) {
+
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+
+
+app.get("/api/notes", function (req, res) {
+    return res.sendFile(path.join(__dirname, "./db/db.json"));
+});
+
+
+
+
+app.post("/api/notes", function (req, res) {
+
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+        if (err) {
+            throw err;
+        }
+        const newnote = JSON.parse(data);
+        req.body.id = newnote.length;
+        newnote.push(req.body);
+        console.log(newnote);
+        fs.writeFile("./db/db.json", JSON.stringify(newnote), (err) => {
+            if (err) {
+                throw err;
+            }
+            res.json({
+                data: newnote
+            })
+        });
+        
+    });
+});
+
+
+
+
+app.delete("/api/notes/:id", function (req, res) {
+    var paramsID = parseInt(req.params.id);
+    // paramsID = paramsID.toString();
+    console.log(paramsID);
+    // var newArray = [];
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+        if (err) {
+            throw err;
+        }
+        var currentData = JSON.parse(data);
+        
+        // req.body.id = newNote.length;
+        // newNote.push(req.body);
+        // console.log(newNote);
+        for (i = 0; i < currentData.length; i++) {
+            // console.log(newNote[i]);
+            if (paramsID === currentData[i].id) {
+                console.log("found it");
+                // i = newNote.length;
+            }
+            else {
+                newArray.push(currentData[i]);
+                console.log(newArray);
+                fs.writeFile("./db/db.json", JSON.stringify(newArray), (err) => {
+                    if (err){
+                        throw err;
+                    }
+                    
+                    console.log(newArray);
+                    res.json(true);
+                });
+               
+               
+            }
+    
+        }
+
+    });
+
+    
+});
